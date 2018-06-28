f060f58a672a54ed66f48f914f22f6a9739dfdbe
kylee <kylee@devband.com>
2018-06-29 00:23:49 +0900

    get_name test

--- contract/board/board.cpp
+++ contract/board/board.cpp
@@ -8,7 +8,9 @@ void board::list(uint64_t page) {
 
 void board::write(string title, string content) {
     account_name writer = get_account();
+    account_name n = get_name();
     print( "Hello, ", name{writer});
+    print( "My name is ", name{n});
 }
 
 void board::read(uint64_t _id) {
